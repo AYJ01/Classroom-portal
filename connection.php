f60f9ed822e8a91d58df6bf6164fb3ee0fca0dcd
Ayyappajith <124246626+AYJ01@users.noreply.github.com>
2025-05-30 22:49:38 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,3 +1,3 @@
 <?php
-$con = new mysqli("localhost",'root','','classroomportal');
+$con = new mysqli("db.be-mons1.bengt.wasmernet.com",'5f1abeb77e2580000efe301b13eb','06835f1a-beb7-7fbf-8000-c69812f30b61','classroomportal');
 ?>
